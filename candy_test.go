c2b92e41d9830b85ec67eea188dee8ce5fc0f1b3
Yi Wang <yi@topic.ai>
2016-01-30 01:09:17 -0800

    Initiail import

---
+++ candy_test.go
@@ -0,0 +1,69 @@
+package candy
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMustWithError(t *testing.T) {
+	func() {
+		defer func() {
+			assert.NotNil(t, recover())
+		}()
+		Must(errors.New("An error"))
+	}()
+
+	func() {
+		defer func() {
+			assert.Nil(t, recover())
+		}()
+		Must(nil)
+	}()
+}
+
+func TestCreateAndOpen(t *testing.T) {
+	filename := path.Join(os.TempDir(),
+		fmt.Sprintf("github.com-augmn-common-candy-%v", time.Now().UnixNano()))
+
+	func() {
+		defer func() {
+			assert.NotNil(t, recover())
+		}()
+		MustOpen(filename)
+	}()
+
+	func() {
+		defer func() {
+			assert.Nil(t, recover())
+		}()
+		CreateAnd(filename, func(w io.Writer) {
+			fmt.Fprintf(w, "Hello!")
+		})
+	}()
+
+	func() {
+		defer func() {
+			assert.Nil(t, recover())
+		}()
+		assert.Equal(t, "Hello!",
+			OpenAnd(filename, func(r io.Reader) interface{} {
+				var x string
+				fmt.Fscanf(r, "%s", &x)
+				return x
+			}).(string))
+	}()
+
+	func() {
+		defer func() {
+			assert.NotNil(t, recover())
+		}()
+		MustCreate("/not-exist-dir/not-exist-file")
+	}()
+}
