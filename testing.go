d8f2147e77a2232e08f6faf36f6c4dd2b626115c
Yi Wang <yi@topic.ai>
2016-01-30 19:39:12 -0800

    More comments

--- testing.go
+++ testing.go
@@ -8,11 +8,20 @@ import (
 	"strings"
 )
 
-// When we do regression test, we often need to build a package
+// GoPath returns the first path in environment GOPATH.
 func GoPath() string {
 	return strings.Split(os.Getenv("GOPATH"), ":")[0]
 }
 
+// TestData returns the full path of the ./testdata sub-directory.  It
+// is supposed to be used in unit tests and works like:
+//
+//    func TestSomethingUsingData(t *testing.T) {
+//        OpenAnd(path.Join(TestData(), "dataset.txt"), func(f io.Reader) {
+//            // f is the file ./testdata/dataset.txt
+//        })
+//    }
+//
 func TestData() string {
 	_, f, _, ok := runtime.Caller(1)
 	if !ok {
